5c390584d77d55e203f39fbd879aba991dac1856
L <EBogatyrevaPK@yandex.ru>
2022-02-15 19:04:16 +1200

    Done)

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -3,6 +3,29 @@ package pro.sky.java.course1;
 public class Main {
 
     public static void main(String[] args) {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String changeNumber = "Иванов Семён Семёнович";
+        String fullName = lastName + " " + firstName + " " + middleName;
 
+        //Задание 1.
+        System.out.println("ФИО сотрудника — " + fullName);
+        //Задание 2.
+        String s = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + s);
+        //Задание 3.
+        s = fullName.replace(" ", "; ");
+        System.out.println("Данные ФИО сотрудника для административного отдела — " + s);
+
+        //Задание 4.
+        //вариант 1.
+        String change = changeNumber.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + change);
+        //вариант 2.
+        if (changeNumber.contains("ё")) {
+            String change1 = changeNumber.replace("ё", "е");
+            System.out.println("Данные ФИО сотрудника — " + change1);
+        }
     }
 }
